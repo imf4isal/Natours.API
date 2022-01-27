4b2e8d0684f3b65f4bfb5d6b387818ec720ecb3b
Faisal <md.faisal1899@yahoo.com>
2022-01-27 19:16:48 +0600

    handled get request to get tours simple data

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 const express = require('express');
-
+const fs = require('fs');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.status(200).send('Hello from the server.');
+// app.get('/', (req, res) => {
+//   res.status(200).send('Hello from the server.');
+// });
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, 'utf-8')
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
 });
 
 const port = 3000;
+
 app.listen(port, () => {
-  console.log(`App is running on port ${port}`);
+  console.log(`app running on the port - ${port}`);
 });
