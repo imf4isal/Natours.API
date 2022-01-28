6cd7d02dc233a2feff23942cfbf749ccf50b3aef
Faisal <md.faisal1899@yahoo.com>
2022-01-28 09:52:25 +0600

    Implemented Patch and Delete Tour Request

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
+// app.use(express.json());
+// // app.get('/', (req, res) => {
+// //   res.status(200).send('Hello from the server.');
+// // });
+
 const express = require('express');
-const fs = require('fs');
 const app = express();
+const fs = require('fs');
 
 app.use(express.json());
-// app.get('/', (req, res) => {
-//   res.status(200).send('Hello from the server.');
-// });
+
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, 'utf-8')
 );
@@ -21,11 +24,16 @@ app.get('/api/v1/tours', (req, res) => {
 });
 
 app.get('/api/v1/tours/:id', (req, res) => {
-  console.log(req.params);
-
   const id = req.params.id * 1;
 
   const tour = tours.find((el) => el.id === id);
+
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Nothing is found.',
+    });
+  }
 
   res.status(200).json({
     status: 'success',
@@ -36,8 +44,9 @@ app.get('/api/v1/tours/:id', (req, res) => {
 });
 
 app.post('/api/v1/tours', (req, res) => {
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
+  const newID = tours[tours.length - 1].id + 1;
+
+  const newTour = Object.assign({ id: newID }, req.body);
 
   tours.push(newTour);
 
@@ -45,18 +54,46 @@ app.post('/api/v1/tours', (req, res) => {
     `${__dirname}/dev-data/data/tours-simple.json`,
     JSON.stringify(tours),
     (err) => {
-      res.status(201).json({
+      res.status(200).json({
         status: 'success',
         data: {
-          tour: newTour,
+          newTour,
         },
       });
     }
   );
 });
 
+app.patch('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Nothing is found.',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    message: 'tour updated..',
+  });
+});
+
+app.delete('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Nothing is found.Invalid tour id.',
+    });
+  }
+
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+});
+
 const port = 3000;
 
 app.listen(port, () => {
-  console.log(`app running on the port - ${port}`);
+  console.log(`app running on port ${port}.. ... listening from server......`);
 });
