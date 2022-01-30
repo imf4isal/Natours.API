f508070032092137e92e2c9d8d22a4dddbf03675
Faisal <md.faisal1899@yahoo.com>
2022-01-30 19:16:02 +0600

    added update and delete controller by mongoose

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -57,20 +57,41 @@ exports.createTour = async (req, res) => {
   }
 };
 
-exports.updateTour = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: '<Updated tour here...>'
-    }
-  });
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    });
+    console.log(tour);
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour: tour
+      }
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'ERROR',
+      message: 'Invalid Data Sent.'
+    });
+  }
 };
 
-exports.deleteTour = (req, res) => {
-  res.status(204).json({
-    status: 'success',
-    data: null
-  });
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+
+    res.status(204).json({
+      status: 'success',
+      data: null
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'ERROR',
+      message: 'Invalid Data Sent.'
+    });
+  }
 };
 
 // const tours = JSON.parse(
