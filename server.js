554d94ea86f7c72b1f159333a11ed8b9a0dbf653
Faisal <md.faisal1899@yahoo.com>
2022-01-30 12:21:13 +0600

    added mongodb and mongoose into natour project and set up atlas cloud database

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+
 dotenv.config({ path: './config.env' });
 const app = require('./app');
+
+const DB = process.env.DATABASE.replace('<password>', process.env.PASSWORD);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(() => console.log('db connection succesful'));
+// console.log(process.env.DATABASE);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
