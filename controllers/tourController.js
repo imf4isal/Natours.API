e34a1669688a244aa2194a4475ce5cab4677523b
Faisal <md.faisal1899@yahoo.com>
2022-01-30 14:42:04 +0600

    added get tour data handler by mongoose

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,27 +1,41 @@
 const Tour = require('./../Model/tourModel');
 
-exports.getAllTours = (req, res) => {
-  // res.status(200).json({
-  //   status: 'success',
-  //   requestedAt: req.requestTime,
-  //   results: tours.length,
-  //   data: {
-  //     tours
-  //   }
-  // });
+exports.getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find();
+
+    res.status(200).json({
+      status: 'success',
+
+      results: tours.length,
+      data: {
+        tours
+      }
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'ERROR',
+      message: 'Invalid Data Sent.'
+    });
+  }
 };
 
-exports.getTour = (req, res) => {
-  // const id = req.params.id * 1;
+exports.getTour = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
 
-  // const tour = tours.find(el => el.id === id);
-
-  res.status(200).json({
-    status: 'success',
-    data: {
-      // tour
-    }
-  });
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour
+      }
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'ERROR',
+      message: 'Invalid Data Sent.'
+    });
+  }
 };
 
 exports.createTour = async (req, res) => {
