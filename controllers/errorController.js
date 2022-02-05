7d4f1e6d927a60e9b2a6f9df2af916709f49ebba
Faisal <md.faisal1899@yahoo.com>
2022-02-05 10:19:26 +0600

    set errors during devlopment and production.different errors for different environment

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,40 @@
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  // Operational error. trusted
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message
+    });
+  } else {
+    // optional. not trusted, dont leak any internal information.
+
+    // log the error
+    console.log('ERROR!!', err);
+
+    //send the error message
+    res.status(500).json({
+      status: 'error',
+      message: 'something is wrong.'
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(err, res);
+  }
 };
