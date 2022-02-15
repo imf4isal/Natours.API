ec1d0dffe83906c9add9f56aa516fe9a3de706c2
Faisal <md.faisal1899@yahoo.com>
2022-02-16 01:32:09 +0600

    added base pug template: header-footer

--- app.js
+++ app.js
@@ -72,7 +72,10 @@ app.use((req, res, next) => {
 // 2) ROUTES
 
 app.get('/', (req, res) => {
-  res.status(200).render('base');
+  res.status(200).render('base', {
+    tour: 'The Sea Explorer',
+    user: 'faisal'
+  });
 });
 
 app.use('/api/v1/tours', tourRouter);
